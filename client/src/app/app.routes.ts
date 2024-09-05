80f17e12a5bb389765e5929defc037bce5b8d9bb
mikezank <mike.zanchettin@gmail.com>
2024-09-05 06:52:48 -0500

    Add member list and detail

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member/member-list/member-list.component';
-import { MemberDetailComponent } from './member/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
@@ -13,7 +13,7 @@ export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: '', runGuardsAndResolvers: 'always', canActivate: [authGuard], children: [
         {path: 'members', component: MemberListComponent},
-        {path: 'members/:id', component: MemberDetailComponent},
+        {path: 'members/:username', component: MemberDetailComponent},
         {path: 'lists', component: ListsComponent},
         {path: 'messages', component: MessagesComponent},
     ]},
